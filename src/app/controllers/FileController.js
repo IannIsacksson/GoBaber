0caf12c05828e7c346843e8613b3682d8cf283fe
iann <iisackssonoliveira@gmail.com>
2019-04-29 20:10:08 -0300

    Finalizou a estilizacao da page dashboard incluindo as imagens dos usuarios

---
+++ src/app/controllers/FileController.js
@@ -0,0 +1,23 @@
+const path = require('path')
+
+class FileController {
+  show (req, res) {
+    const { file } = req.params
+    console.log(req.params)
+    // Retorna o caminho para acessar as imagens
+    // Ex.:C:\code\go-stack\node\modulo2\tmp\uploads\aac29aec6d0763137c2572c6f7e94b9c.jpg
+    const filePath = path.resolve(
+      __dirname,
+      '..',
+      '..',
+      '..',
+      'tmp',
+      'uploads',
+      file
+    )
+
+    return res.sendFile(filePath)
+  }
+}
+
+module.exports = new FileController()
