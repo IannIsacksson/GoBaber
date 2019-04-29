5ffcd7f14e97783f4e04f0fb9a1d7a82a6e85533
iann <iisackssonoliveira@gmail.com>
2019-04-29 20:39:05 -0300

    Model e Migration de agendamento

---
+++ src/app/models/Appointment.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, DataTypes) => {
+  const Appointment = sequelize.define('Appointment', {
+    date: DataTypes.DATE
+  })
+
+  // Relacionamento com a mesma tabela User.
+  // O belongsTo é usado para armazenar o relacionamento na mesma tabela, neste caso Appointment.
+  Appointment.associate = models => {
+    Appointment.belongsTo(models.User, { foreignKey: 'user_id' })
+    Appointment.belongsTo(models.User, { foreignKey: 'provider_id' })
+  }
+  // Para criar a migration é preciso criar o model
+  // Depois rodar o: npx sequelize migration:create --name=create-appointments
+  return Appointment
+}
